3622f267054158577626dc3105638b6cc7238c31
ankita-toqnoq <ankitap@toqnoq.com>
2026-09-17 18:04:28 +0530

    add a print statement

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ def fibonacci(n):
 
 
 fibonacci(10)
+
+
+print("\nFibonacci (original):", end=" ")
+a = 0
+b = 1
+for i in range(10):
+    print(a, end=" ")
+    a, b = b, a + b 
+
+
+
+
+print("hello toqnoq")    
